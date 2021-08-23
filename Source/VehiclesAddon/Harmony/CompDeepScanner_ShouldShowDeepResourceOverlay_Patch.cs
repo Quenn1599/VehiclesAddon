c4640a54be77f81668d58e43938701aab54a1c92
Quenn1599 <quenn1599@gmail.com>
2021-08-23 16:41:46 -0500

    Deepscanner transpiler patch

---
+++ Source/VehiclesAddon/Harmony/CompDeepScanner_ShouldShowDeepResourceOverlay_Patch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Quenn1599.VehiclesAddon
+{
+    // Messy transpiler patch, hopefully no one else is touching this method
+    [HarmonyPatch(typeof(CompDeepScanner))]
+    [HarmonyPatch("ShouldShowDeepResourceOverlay")]
+    public static class CompDeepScanner_ShouldShowDeepResourceOverlay_Patch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (CodeInstruction instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Ldc_I4_0)
+                {
+                    yield return new CodeInstruction(OpCodes.Ldc_I4_1);
+                } else
+                {
+                    yield return instruction;
+                }
+            }
+        }
+    }
+}
