8bbdd76f0180b11efa22360fe8f5f35b35c17ea0
Quenn1599 <quenn1599@gmail.com>
2021-08-23 16:41:57 -0500

    Harmony patch setup

---
+++ Source/VehiclesAddon/Harmony/HarmonyPatches.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Verse;
+using HarmonyLib;
+
+namespace Quenn1599.VehiclesAddon
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches {
+        static HarmonyPatches()
+        {
+            var harmony = new Harmony("quenn1599.vehiclesaddon");
+            harmony.PatchAll();
+        }
+    }
+}
