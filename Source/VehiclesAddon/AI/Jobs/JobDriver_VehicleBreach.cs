d2380afe2b1d7399b9d431c4b286cdbc472b1c3a
Quenn1599 <quenn1599@gmail.com>
2021-08-25 22:45:47 -0500

    Added job classes for vehicle breaching behaviors.

---
+++ Source/VehiclesAddon/AI/Jobs/JobDriver_VehicleBreach.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI;
+using RimWorld;
+using Vehicles;
+
+namespace Quenn1599.VehiclesAddon
+{
+    class JobDriver_VehicleBreach : JobDriver
+    {
+        private Thing BreachTarget
+        {
+            get
+            {
+                return this.job.GetTarget(TargetIndex.A).Thing;
+            }
+        }
+        private VehiclePawn vehicle;
+
+        public override bool TryMakePreToilReservations(bool errorOnFailed)
+        {
+            return true;
+        }
+
+        protected override IEnumerable<Toil> MakeNewToils()
+        {
+            this.FailOnDespawnedNullOrForbidden(TargetIndex.A);
+
+            yield return Toils_Goto.GotoThing(TargetIndex.A, PathEndMode.Touch);
+
+            Toil breach = new Toil();
+            breach.tickAction = delegate ()
+            {
+                if (breach.actor is VehiclePawn vehicle) {
+                    Thing target = this.BreachTarget;
+                    if (this.ticksToNextHit-- <= 0)
+                    {
+                        IntVec3 position = BreachTarget.Position;
+                        if (this.effecter == null)
+                        {
+                            this.effecter = EffecterDefOf.Mine.Spawn();
+                        }
+                        this.effecter.Trigger(breach.actor, BreachTarget);
+                        
+                        if (BreachTarget.def.useHitPoints)
+                        {
+                            Mineable mineable = BreachTarget as Mineable;
+                            if (mineable == null || BreachTarget.HitPoints > DefaultDamagePerHit)
+                            {
+                                DamageInfo dinfo = new DamageInfo(DamageDefOf.Mining, (float)DefaultDamagePerHit, 0f, -1f, breach.actor, null, null, DamageInfo.SourceCategory.ThingOrUnknown, null, true, true);
+                                BreachTarget.TakeDamage(dinfo);
+                            } else
+                            {
+                                mineable.Notify_TookMiningDamage(BreachTarget.HitPoints, breach.actor);
+                                mineable.HitPoints = 0;
+                                mineable.DestroyMined(breach.actor);
+                            }
+                        } else
+                        {
+                            // Destroy object if not using hp
+                            BreachTarget.Destroy(DestroyMode.KillFinalize);
+                        }
+                        if (BreachTarget.Destroyed)
+                        {
+                            breach.actor.Map.mineStrikeManager.CheckStruckOre(position, BreachTarget.def, breach.actor);
+                            if (this.pawn.Faction != Faction.OfPlayer)
+                            {
+                                List<Thing> thingList = position.GetThingList(this.Map);
+                                for (int i = 0; i < thingList.Count; i++)
+                                {
+                                    thingList[i].SetForbidden(true, false);
+                                }
+                            }
+                            this.ReadyForNextToil();
+                            return;
+                        }
+                    }
+                } else
+                {
+                    Log.Error("For some reason we're trying to do vehicle mining with a non-vehicle pawn. How did this happen??");
+                    this.ReadyForNextToil();
+                    return;
+                }
+            };
+            breach.defaultCompleteMode = ToilCompleteMode.Never;
+            if (BreachTarget.def.useHitPoints)
+            {
+                breach.WithProgressBar(TargetIndex.A, () => 1f - (float)this.BreachTarget.HitPoints / (float)this.BreachTarget.MaxHitPoints, false, -0.5f, false);
+            }
+            breach.FailOnCannotTouch(TargetIndex.A, PathEndMode.Touch);
+            yield return breach;
+            yield break;
+        }
+
+        private void RestTicksToNextHit()
+        {
+            this.ticksToNextHit = DefaultTicksBetweenHits;
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look<int>(ref this.ticksToNextHit, "ticksToNextHit", 0, false);
+        }
+
+        private int ticksToNextHit = -1000;
+
+        private Effecter effecter;
+
+        public const int DefaultTicksBetweenHits = 10;
+
+        public const int DefaultDamagePerHit = 100;
+    }
+}
---
+++ Source/VehiclesAddon/AI/Jobs/JobGiver_VehicleBreach.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Verse;
+using RimWorld;
+using Verse.AI;
+using Vehicles;
+using Vehicles.AI;
+using SmashTools;
+
+namespace Quenn1599.VehiclesAddon
+{
+    // TODO: Finish adapting logic for detecting what to give breach jobs for and when to use goto jobs
+    public class JobGiver_VehicleBreach : ThinkNode_JobGiver
+    {
+        public override ThinkNode DeepCopy(bool resolve = true)
+        {
+            JobGiver_VehicleBreach jobgiver = (JobGiver_VehicleBreach)base.DeepCopy(resolve);
+            jobgiver.canMineMineables = this.canMineMineables;
+            jobgiver.canMineNonMineables = this.canMineNonMineables;
+            return jobgiver;
+        }
+
+        protected override Job TryGiveJob(Pawn pawn)
+        {
+            if (pawn is VehiclePawn vehicle)
+            {
+                CompVehicleBreacher breacher = vehicle.TryGetComp<CompVehicleBreacher>();
+                if (breacher != null)
+                {
+                    IntVec3 intVec3 = vehicle.mindState.duty.focus.Cell;
+                    if (vehicle.Position == intVec3)
+                    {
+                        // Destination reached
+                        return null;
+                    }
+
+                    int num = GenRadial.NumCellsInRadius(2.9f);
+                    int i = 0;
+                    IntVec3 currentPos;
+                    while (i < num)
+                    {
+                        currentPos = GenRadial.RadialPattern[i] + intVec3;
+                        if (VehicleReachabilityUtility.CanReachVehicle(vehicle, currentPos, PathEndMode.OnCell, Danger.Deadly, false, TraverseMode.PassAllDestroyableThings))
+                        {
+                            PawnPath result;
+                            // Threaded pathfinding
+                            var cts = new CancellationTokenSource();
+                            try
+                            {
+                                var tasks = new[]
+                                {
+                                    Task<(PawnPath path, bool found)>.Factory.StartNew(() => vehicle.Map.GetCachedMapComponent<VehicleMapping>()[vehicle.VehicleDef].VehiclePathFinder.FindVehiclePath(vehicle.Position, currentPos, TraverseParms.For(TraverseMode.PassAllDestroyableThings, Danger.Deadly, false, false, false), cts.Token),cts.Token)
+                                };
+                                int taskIndex = Task.WaitAny(tasks, cts.Token);
+
+                                if (tasks[taskIndex].Result.ToTuple().Item1 != null && !tasks[taskIndex].Result.ToTuple().Item1.Found && !tasks[taskIndex].Result.ToTuple().Item2)
+                                {
+                                    try
+                                    {
+                                        cts.Cancel();
+                                        cts.Dispose();
+                                        // No path :(
+                                        return null;
+                                    } catch (Exception ex)
+                                    {
+                                        Log.Error("Something has gone wrong with cancelling a pathfinding task.");
+                                    }
+                                }
+                                result = tasks[0].Result.ToTuple().Item1;
+                            } catch (AggregateException ex)
+                            {
+                                Log.Error("A cosmic entity has tampered with the threads. This is not good.");
+                                cts.Cancel();
+                                cts.Dispose();
+                                return null;
+                            }
+                            IntVec3 cellBeforeBlocker;
+                            Thing thing = result.FirstBlockingBuildingVehicle(out cellBeforeBlocker, vehicle);
+                            
+                        }
+                        else
+                        {
+                            i++;
+                        }
+                        
+                    }
+                    return null;
+                }
+            }
+        }
+
+        private bool canMineMineables = true;
+        private bool canMineNonMineables = true;
+    }
+}
