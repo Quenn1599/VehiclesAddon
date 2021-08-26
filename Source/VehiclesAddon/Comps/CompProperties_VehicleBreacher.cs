ac2223ff866fe4034d8f4828b291dd885ffb122f
Quenn1599 <quenn1599@gmail.com>
2021-08-25 22:46:00 -0500

    Vehicle breaching comp dummy classes.

---
+++ Source/VehiclesAddon/Comps/CompProperties_VehicleBreacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Quenn1599.VehiclesAddon
+{
+    public class CompProperties_VehicleBreacher : CompProperties
+    {
+        public CompProperties_VehicleBreacher()
+        {
+            this.compClass = typeof(CompVehicleBreacher);
+        }
+    }
+}
