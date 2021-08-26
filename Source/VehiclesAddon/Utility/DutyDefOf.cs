6334d66d1377d18391fe51faf4348b9816f746e9
Quenn1599 <quenn1599@gmail.com>
2021-08-25 22:47:02 -0500

    Added DutyDefOf just so I can refer to the breaching duty easily.

---
+++ Source/VehiclesAddon/Utility/DutyDefOf.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using Verse.AI;
+
+namespace Quenn1599.VehiclesAddon
+{
+    [DefOf]
+    public static class DutyDefOf
+    {
+        static DutyDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(DutyDefOf));
+        }
+
+        public static DutyDef VehicleBreach;
+    }
+}
